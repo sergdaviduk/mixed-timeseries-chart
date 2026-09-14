b4463a223560acdaf19da23cc59e3d82e3e69923
sergdaviduk <sergey-daviduk@mail.ru>
2026-09-14 13:51:47 +0300

    Add demo time-series data

---
+++ src/data.ts
@@ -0,0 +1,29 @@
+import type { TimeSeriesPoint } from './types';
+
+export const areaData: TimeSeriesPoint[] = [
+  { timestamp: '2026-06-10', value: 0 },
+  { timestamp: '2026-06-11', value: 18 },
+  { timestamp: '2026-06-12', value: 44.36 },
+  { timestamp: '2026-06-13', value: 58 },
+];
+
+export const splineData: TimeSeriesPoint[] = [
+  { timestamp: '2026-06-10', value: 72 },
+  { timestamp: '2026-06-11', value: 26 },
+  { timestamp: '2026-06-12', value: 21 },
+  { timestamp: '2026-06-13', value: 46 },
+];
+
+export const lineData: TimeSeriesPoint[] = [
+  { timestamp: '2026-06-10', value: 2 },
+  { timestamp: '2026-06-11', value: 24 },
+  { timestamp: '2026-06-12', value: 36 },
+  { timestamp: '2026-06-13', value: 66 },
+];
+
+export const barData: TimeSeriesPoint[] = [
+  { timestamp: '2026-06-10', value: 1.1 },
+  { timestamp: '2026-06-11', value: 1.23 },
+  { timestamp: '2026-06-12', value: 1.23 },
+  { timestamp: '2026-06-13', value: 1.18 },
+];
