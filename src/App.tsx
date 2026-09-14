cc499e55bd93a813ce6c293c1357901c0935577e
sergdaviduk <sergey-daviduk@mail.ru>
2026-09-14 13:58:26 +0300

    Match reference chart proportions

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import { areaData, barData, lineData, splineData } from './data';
 export default function App() {
   return (
     <main className="page">
-      <section className="demo-card">
+      <div className="chart-demo">
         <MixedTimeSeriesChart
           area={areaData}
           spline={splineData}
           line={lineData}
           bar={barData}
+          height={529}
         />
-      </section>
+      </div>
     </main>
   );
 }
